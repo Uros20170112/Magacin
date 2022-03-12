a42531fb5b2af7ae65f5a4ee3b3a61cefe6e3ba6
Uros Soljaga <uros.soljaga1998@gmail.com>
2022-03-12 22:02:10 +0100

    Dodate equals, hashCode i toString metode u klase Knjiga, KancelarijskiMaterija i KucnaHemija

--- Magacin/src/artikal/KancelarijskiMaterijal.java
+++ Magacin/src/artikal/KancelarijskiMaterijal.java
@@ -29,6 +29,32 @@ public class KancelarijskiMaterijal {
 	public void setKolicina(int kolicina) {
 		this.kolicina = kolicina;
 	}
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + sifra;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KancelarijskiMaterijal other = (KancelarijskiMaterijal) obj;
+		if (sifra != other.sifra)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "KancelarijskiMaterijal [naziv=" + naziv + ", sifra=" + sifra + ", opis=" + opis + ", kolicina="
+				+ kolicina + "]";
+	}
+
+
 	
 }
--- Magacin/src/artikal/Knjiga.java
+++ Magacin/src/artikal/Knjiga.java
@@ -29,6 +29,30 @@ public class Knjiga {
 	public void setKolicina(int kolicina) {
 		this.kolicina = kolicina;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + sifra;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (sifra != other.sifra)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Knjiga [naziv=" + naziv + ", sifra=" + sifra + ", opis=" + opis + ", kolicina=" + kolicina + "]";
+	}
 	
 	
 }
