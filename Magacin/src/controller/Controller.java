bda22425458ed5519f013963d0fdc9d493432a96
Uros Soljaga <uros.soljaga1998@gmail.com>
2022-03-12 23:02:52 +0100

    Dodatak klasa Controller i implementirane sve metode iz MagacinInterfejs

---
+++ Magacin/src/controller/Controller.java
@@ -0,0 +1,48 @@
+package controller;
+
+import artikal.KancelarijskiMaterijal;
+import artikal.Knjiga;
+import artikal.KucnaHemija;
+
+public class Controller {
+
+	public void dodajKnjigu(Knjiga knjiga) {
+	
+	}
+	
+	public void obrisiKnjigu(Knjiga knjiga) {
+		
+	}
+	
+	public Knjiga pronadjiKnjigu(int sifra) {
+		return null;
+	}
+	
+	
+	
+	public void dodajKancelarijskiMaterijal(KancelarijskiMaterijal km) {
+		
+	}
+	
+	public void obrisiKnjigu(KancelarijskiMaterijal km) {
+		
+	}
+	
+	public KancelarijskiMaterijal pronadjiKancelarijskiMaterijal(int sifra) {
+		return null;
+	}
+	
+	
+	
+	public void dodajKucnuHemiju(KucnaHemija kh) {
+		
+	}
+	
+	public void obrisiKucnuHemiju(KucnaHemija kh) {
+		
+	}
+	
+	public KucnaHemija pronadjiKucnuHemiju(int sifra) {
+		return null;
+	}
+}
