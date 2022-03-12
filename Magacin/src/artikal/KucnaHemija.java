3632c7f823c85b598c18c03984ed5024e62b2a46
Uros Soljaga <uros.soljaga1998@gmail.com>
2022-03-12 22:50:59 +0100

    Dodata logicka kontrola za sve set metode

--- Magacin/src/artikal/KucnaHemija.java
+++ Magacin/src/artikal/KucnaHemija.java
@@ -12,30 +12,51 @@ public class KucnaHemija {
 		return naziv;
 	}
 	public void setNaziv(String naziv) {
+		if(naziv == null) {
+			throw new NullPointerException("Naziv ne sme da bude null!");
+		}
+		if(naziv == "" || naziv == " ") {
+			throw new IllegalArgumentException("Naziv ne sme da bude prazan string!");
+		}
 		this.naziv = naziv;
 	}
 	public int getSifra() {
 		return sifra;
 	}
 	public void setSifra(int sifra) {
+		if(sifra < 0 ) {
+			throw new IllegalArgumentException("Sifra mora biti pozitivan broj!");
+		}
 		this.sifra = sifra;
 	}
 	public String getOpis() {
 		return opis;
 	}
 	public void setOpis(String opis) {
+		if(opis == null) {
+			throw new NullPointerException("Naziv ne sme da bude null!");
+		}
+		if(opis.length() < 10) {
+			throw new IllegalArgumentException("Opis mora da ima vise od 10 karaktera!");
+		}
 		this.opis = opis;
 	}
 	public int getKolicina() {
 		return kolicina;
 	}
 	public void setKolicina(int kolicina) {
+		if(kolicina < 0) {
+			throw new IllegalArgumentException("Kolicina ne sme da bude manja od 0!");
+		}
 		this.kolicina = kolicina;
 	}
 	public LocalDate getRokTrajanja() {
 		return rokTrajanja;
 	}
 	public void setRokTrajanja(LocalDate rokTrajanja) {
+		if(rokTrajanja == null) {
+			throw new NullPointerException("Rok trajanja ne sme da bude null!");
+		}
 		this.rokTrajanja = rokTrajanja;
 	}
 	@Override
